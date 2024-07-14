4b2ae35a114ca1652ec8e4797e1c947d9361b8e6
Dylan Drendel <dylan.drendel@gmail.com>
2024-07-14 16:17:14 -0600

    small changes

--- app/pocketbase.ts
+++ app/pocketbase.ts
@@ -1,3 +1,4 @@
-import PocketBase from 'pocketbase';
-export const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL || 'http://127.0.0.1:8090')
-;
+import PocketBase from "pocketbase";
+export const pb = new PocketBase(
+  process.env.NEXT_PUBLIC_POCKETBASE_URL || "http://127.0.0.1:8090"
+);
